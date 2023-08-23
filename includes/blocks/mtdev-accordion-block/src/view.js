a36d6c754fe6e3693bb33909b02cfca967ec652e
Marta Torre <hola@martatorre.dev>
2023-08-22 22:18:22 -0400

    fix(AccordionBlock): Max heigth logic

--- includes/blocks/mtdev-accordion-block/src/view.js
+++ includes/blocks/mtdev-accordion-block/src/view.js
@@ -1,3 +1,7 @@
+import { arrowString } from './Arrow'
+
+const ERROR_MARGIN = 30
+
 window.addEventListener('DOMContentLoaded', () => {
   const accordions = Array.from(document.querySelectorAll('.mtdev-accordion'))
 
@@ -5,7 +9,15 @@ window.addEventListener('DOMContentLoaded', () => {
     const accordionHeader = accordion.querySelector('.mtdev-accordion__header')
 
     accordionHeader.addEventListener('click', () => {
-      accordion.classList.toggle('is-showed')
+      const isOpen = accordion.classList.contains('is-open')
+      const content = accordionHeader.nextElementSibling
+
+      const maxHeight = isOpen ? '0px' : `${content.scrollHeight + ERROR_MARGIN}px`
+      content.style.maxHeight = maxHeight
+
+      accordion.classList.toggle('is-open')
     })
+
+    accordionHeader.insertAdjacentHTML('beforeend', arrowString)
   })
 })
