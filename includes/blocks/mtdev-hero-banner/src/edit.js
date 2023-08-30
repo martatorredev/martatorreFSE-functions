830d71907955825535ce9ec05b06635e12c1f548
Marta Torre <hola@martatorre.dev>
2023-08-29 20:37:48 -0400

    feat(HeroBannerBlock): Add a control to hide title and subtitle

--- includes/blocks/mtdev-hero-banner/src/edit.js
+++ includes/blocks/mtdev-hero-banner/src/edit.js
@@ -2,9 +2,14 @@ import {
   RichText,
   useBlockProps,
   BlockControls,
-  InnerBlocks
+  InnerBlocks,
+  InspectorControls
 } from '@wordpress/block-editor'
-import { ToggleControl } from '@wordpress/components'
+import {
+  ToggleControl,
+  PanelBody
+} from '@wordpress/components'
+
 import HeadingLevelDropdown from './heading-level-dropdown'
 import background from './img/fondopixeles.png'
 
@@ -13,34 +18,50 @@ const Edit = ({ attributes, setAttributes }) => {
 
   return (
     <>
-      <BlockControls group='block'>
-        <HeadingLevelDropdown
-          value={titleLevel}
-          onChange={titleLevel => setAttributes({ titleLevel })}
-        />
-        {/* Show only inner blocks toggle */}
-        <ToggleControl
-          label='Mostrar solo bloques internos'
-          checked={showOnlyInnerBlocks}
-          onChange={showOnlyInnerBlocks => setAttributes({ showOnlyInnerBlocks })}
-        />
-      </BlockControls>
+      <InspectorControls>
+        <PanelBody title='Opciones'>
+          <ToggleControl
+            label='Mostrar solo bloques internos'
+            checked={showOnlyInnerBlocks}
+            onChange={showOnlyInnerBlocks => setAttributes({ showOnlyInnerBlocks })}
+          />
+        </PanelBody>
+      </InspectorControls>
+
+      {
+        !showOnlyInnerBlocks && (
+          <BlockControls group='block'>
+            <HeadingLevelDropdown
+              value={titleLevel}
+              onChange={titleLevel => setAttributes({ titleLevel })}
+            />
+          </BlockControls>
+        )
+      }
       <header {...useBlockProps({ className: 'hero-banner' })}>
         <section className='hero-banner__content'>
-          <RichText
-            tagName={`h${titleLevel}`}
-            className='hero-banner__title'
-            value={title}
-            onChange={title => setAttributes({ title })}
-            placeholder='Titulo principal...'
-          />
-          <RichText
-            tagName='p'
-            className='hero-banner__subtitle'
-            value={subtitle}
-            onChange={subtitle => setAttributes({ subtitle })}
-            placeholder='Subtitulo...'
-          />
+          {
+            !showOnlyInnerBlocks && (
+              <>
+                <RichText
+                  tagName={`h${titleLevel}`}
+                  className='hero-banner__title'
+                  value={title}
+                  onChange={title => setAttributes({ title })}
+                  placeholder='Titulo principal...'
+                />
+
+                <RichText
+                  tagName='p'
+                  className='hero-banner__subtitle'
+                  value={subtitle}
+                  onChange={subtitle => setAttributes({ subtitle })}
+                  placeholder='Subtitulo...'
+                />
+              </>
+            )
+          }
+
           <InnerBlocks />
         </section>
         <img
