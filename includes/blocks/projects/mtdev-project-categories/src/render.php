5ebd1b58edd1123d379ca5f07c9e8bade583f872
Marta Torre <hola@martatorre.dev>
2023-09-02 20:27:20 -0400

    feat: Project categories block

---
+++ includes/blocks/projects/mtdev-project-categories/src/render.php
@@ -0,0 +1,26 @@
+<?php
+
+$args = array(
+  'taxonomy'     => 'mis_proyectos_web_category',
+  'echo'         => false,
+  'hierarchical' => ! empty( $attributes['showHierarchy'] ),
+  'orderby'      => 'name',
+  'show_count'   => ! empty( $attributes['showPostCounts'] ),
+  'title_li'     => '',
+  'hide_empty'   => empty( $attributes['showEmpty'] ),
+);
+if ( ! empty( $attributes['showOnlyTopLevel'] ) && $attributes['showOnlyTopLevel'] ) {
+  $args['parent'] = 0;
+}
+
+$wrapper_markup = '<ul %1$s>%2$s</ul>';
+$items_markup   = wp_list_categories( $args );
+$type           = 'list';
+
+$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => "mtdev-block-categories-{$type}" ) );
+
+return sprintf(
+  $wrapper_markup,
+  $wrapper_attributes,
+  $items_markup
+);
