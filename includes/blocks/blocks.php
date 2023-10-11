e622e6c0c592ae2e089a35af99320488ee6583f5
Marta Torre <hola@martatorre.dev>
2023-10-10 22:47:52 -0400

    feat: Require new block

--- includes/blocks/blocks.php
+++ includes/blocks/blocks.php
@@ -1,6 +1,7 @@
 <?php
 require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/mtdev-hero-banner/mtdev-hero-banner.php';
 require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/mtdev-testimonials-block/mtdev-testimonials-block.php';
+require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/mtdev-colaborations-block/mtdev-colaborations-block.php';
 require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/mtdev-accordion-block/mtdev-accordion-block.php';
 require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/mtdev-timeline-block/mtdev-timeline-block.php';
 require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/posts/mtdev-latest-posts/mtdev-latest-posts.php';
