673bd8316c469e019f06bb7c0276560521470f0e
Marta Torre <hola@martatorre.dev>
2023-10-18 22:03:52 -0400

    feat: Require new blocks

--- includes/blocks/blocks.php
+++ includes/blocks/blocks.php
@@ -7,7 +7,9 @@ require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/mtdev-timeline-block/m
 require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/posts/mtdev-latest-posts/mtdev-latest-posts.php';
 require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/posts/mtdev-main-term/mtdev-main-term.php';
 require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/posts/mtdev-post-time-to-read/mtdev-post-time-to-read.php';
-require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/posts/mtdev-related-posts/mtdev-related-posts.php' ;
+require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/posts/mtdev-related-posts/mtdev-related-posts.php';
+require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/presentations/mtdev-related-presentations/mtdev-related-presentations.php';
+require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/presentations/mtdev-latest-presentations/mtdev-latest-presentations.php';
 require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/projects/mtdev-latest-projects/mtdev-latest-projects.php';
 require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/projects/mtdev-related-projects/mtdev-related-projects.php';
 require_once MTDEV_EXTRAS_PLUGIN_PATH . '/includes/blocks/projects/mtdev-project-categories/mtdev-project-categories.php';
