12210d80377ff19a238cfc9389c1834d33691023
Marta Torre <hola@martatorre.dev>
2023-10-21 10:41:16 -0400

    feat: Change accordion block

--- includes/blocks/mtdev-accordion-block/src/view.js
+++ includes/blocks/mtdev-accordion-block/src/view.js
@@ -1,23 +1,28 @@
 import { arrowString } from './Arrow'
+import jQuery from 'jquery'
+import 'jquery-ui/ui/widgets/accordion.js'
 
-const ERROR_MARGIN = 30
+const ACCORDION_OPTIONS = {
+  header: '.mtdev-accordion__header',
+  collapsible: true,
+  active: false,
+  heightStyle: 'content',
+  icons: null
+}
 
 window.addEventListener('DOMContentLoaded', () => {
   const accordions = Array.from(document.querySelectorAll('.mtdev-accordion'))
+  const [firstAccordion] = accordions
+  const isFaq = !!firstAccordion?.parentElement?.classList?.contains('wp-block-group')
+  // ? If isFaqs is true, then we are in the FAQs group block, so we need to initialize the accordion on the parent element
 
   accordions.forEach(accordion => {
     const accordionHeader = accordion.querySelector('.mtdev-accordion__header')
 
-    accordionHeader.addEventListener('click', () => {
-      const isOpen = accordion.classList.contains('is-open')
-      const content = accordionHeader.nextElementSibling
-
-      const maxHeight = isOpen ? '0px' : `${content.scrollHeight + ERROR_MARGIN}px`
-      content.style.maxHeight = maxHeight
-
-      accordion.classList.toggle('is-open')
-    })
+    if (!isFaq) jQuery(accordion).accordion(ACCORDION_OPTIONS)
 
     accordionHeader.insertAdjacentHTML('beforeend', arrowString)
   })
+
+  if (isFaq) jQuery(firstAccordion.parentElement).accordion(ACCORDION_OPTIONS)
 })
